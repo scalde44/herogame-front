6713a33913629dd99370beee3f34019536ec2e4b
Sebastian Pineda <63763483+sebascamilo12@users.noreply.github.com>
2022-07-23 13:30:04 -0500

    Se crea componente para actualizar carta

---
+++ src/app/feature/card/components/update-card/update-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-card',
+  templateUrl: './update-card.component.html',
+  styleUrls: ['./update-card.component.scss']
+})
+export class UpdateCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
