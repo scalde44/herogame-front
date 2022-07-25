773c098e72779ca58b7cdded10fe3189815ce566
JosueArdila <42660657+JosueArdila@users.noreply.github.com>
2022-07-25 15:18:45 -0500

    variables de entorno necesarias configuradas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  firebaseConfig : {
+    apiKey: "AIzaSyDvw78bgWNKm7cAKe4i1HHuQIN_atsRSbU",
+    authDomain: "herogamesofkau.firebaseapp.com",
+    projectId: "herogamesofkau",
+    storageBucket: "herogamesofkau.appspot.com",
+    messagingSenderId: "457935313208",
+    appId: "1:457935313208:web:ff61bd8c7745a120e649cd"
+  },
+  production: true,
+  apiUrl:'http://localhost:8080/api/v1'
 };
